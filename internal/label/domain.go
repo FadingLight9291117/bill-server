71e25bfa9dd1257fcd922f4ccd727640a80f7b4d
clz <fadinglight9291117@126.com>
2023-05-31 16:36:45 +0800

    feat: label domain

---
+++ internal/label/domain.go
@@ -0,0 +1,27 @@
+package label
+
+import "context"
+
+type Label struct {
+	ID         int    `json:"id"`
+	Type       string `json:"type"`
+	Name       string `json:"name"`
+	RelativeId int    `json:"relativeId"`
+	Count      int    `json:"count"`
+}
+
+type LabelRepository interface {
+	GetLabels(ctx context.Context) (*[]Label, error)
+	GetLabelById(ctx context.Context, id int) (*Label, error)
+	CreateLabel(ctx context.Context, label *Label) error
+	UpdateLabel(ctx context.Context, label *Label) error
+	DeleteLabel(ctx context.Context, id int) error
+}
+
+type LabelService interface {
+	GetLabels(ctx context.Context) (*[]Label, error)
+	GetLabelById(ctx context.Context, id int) (*Label, error)
+	CreateLabel(ctx context.Context, label *Label) error
+	UpdateLabel(ctx context.Context, label *Label) error
+	DeleteLabel(ctx context.Context, id int) error
+}
