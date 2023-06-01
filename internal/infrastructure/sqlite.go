49a21a04498b3c73a90118d2896ee51fb925eaf5
Liangzhao Che <FadingLight9291117@126.com>
2023-06-01 20:07:02 +0800

    feat: add migrate sql data

--- internal/infrastructure/sqlite.go
+++ internal/infrastructure/sqlite.go
@@ -5,6 +5,7 @@ import (
 	"bill-go-fiber/internal/label"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"os"
 )
 
 func ConnectToSqlite() (*gorm.DB, error) {
@@ -24,7 +25,14 @@ func ConnectToSqlite() (*gorm.DB, error) {
 		panic("failed to migrate database.\n")
 	}
 
-	db.Create(&label.Label{ID: 1, Name: "bill"})
+	// migrate data
+	go func() {
+		sql, err := os.ReadFile("scripts/sqliteMigrations.sql")
+		if err != nil {
+			panic("failed to read migration sql file: " + err.Error())
+		}
+		db.Exec(string(sql))
+	}()
 
 	return db, nil
 }
---
+++ scripts/sqliteMigrations.sql
@@ -0,0 +1,8 @@
+INSERT INTO labels (id, type, name, count)
+VALUES (1, '', 'bill', 0),
+       (2, '', 'bill', 0),
+       (3, '', 'bill', 0),
+       (4, '', 'bill', 0),
+       (5, '', 'bill', 0),
+       (6, '', 'bill', 0),
+       (7, '', 'bill', 0);
